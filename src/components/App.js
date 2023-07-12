33fa4a7eae6ca12700d42655f8245b980613096e
kamrankhan0001 <kamranbgp15@gmail.com>
2023-07-12 13:24:44 +0530

    submitting code-Wed Jul 12 2023 13:24:43 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -8,18 +8,36 @@ import './../styles/App.css';
 
     const [inputValue, setInputValue] = useState("");
 
-    
-    return (
-      <div>
-          {/* Do not remove the main div */}
-          <h1>Parent Component</h1>
-          <h2>Child Component</h2>
-          <input type="text" placeholder="Write Something Here" onChange={(e) => setInputValue(e.target.value)}/>
-          <p>{inputValue}</p>
+    const handleInputChange = (e)=>{
+      setInputValue(e.target.value);
+    }
+
+    return(
+      <div className="parent">
+       <Child inputValue={inputValue} handleInputChange={handleInputChange}/>
       </div>
     )
+    }
 
-  }
+    const Child = ({inputValue, handleInputChange})=>{
+      return (
+        <div className="child">
+            {/* Do not remove the main div */}
+            <h1>Parent Component</h1>
+            <h2>Child Component</h2>
+            <input type="text" placeholder="Write Something Here" onChange={handleInputChange}/>
+            
+        </div>
+      )
+      
+
+    }
+
+    export default Parent;
+    
+    
+
+  
   
 
 
