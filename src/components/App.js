a59d3ab84659dfd659b225db562bc7ce8fe391ee
kamrankhan0001 <kamranbgp15@gmail.com>
2023-07-12 12:44:41 +0530

    submitting code-Wed Jul 12 2023 12:44:39 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
-const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
 
-export default App
+
+  const Parent = () =>{
+
+    const [inputValue, setinputvalue] = useState("");
+
+    
+    return (
+      <div>
+          {/* Do not remove the main div */}
+          <h1>Parent Component</h1>
+          <h2>Child Component</h2>
+          <input type="text" placeholder="Write Something Here" onChange={(e) => setinputvalue(e.target.value)}/>
+          <p>{inputValue}</p>
+      </div>
+    )
+
+  }
+  
+
+
+
