dfc5f7d3def1c0fc4bd36ec422ec975214ab61c7
kamrankhan0001 <kamranbgp15@gmail.com>
2023-07-12 12:55:41 +0530

    submitting code-Wed Jul 12 2023 12:55:40 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import './../styles/App.css';
 
   const Parent = () =>{
 
-    const [inputValue, setinputvalue] = useState("");
+    const [inputValue, setInputValue] = useState("");
 
     
     return (
@@ -14,7 +14,7 @@ import './../styles/App.css';
           {/* Do not remove the main div */}
           <h1>Parent Component</h1>
           <h2>Child Component</h2>
-          <input type="text" placeholder="Write Something Here" onChange={(e) => setinputvalue(e.target.value)}/>
+          <input type="text" placeholder="Write Something Here" onChange={(e) => setInputValue(e.target.value)}/>
           <p>{inputValue}</p>
       </div>
     )
